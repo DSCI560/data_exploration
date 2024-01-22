7c0c151562784d0e92e5f2834e6876073a6001a5
Cici Chang <cicichan@usc.edu>
2024-01-21 20:55:31 -0800

    code edited

--- data_exploration.py
+++ data_exploration.py
@@ -8,39 +8,34 @@ import re
 
 
 # 1. Get Data from API as CSV files
-# API endpoint URL
+# request sent to url
 url = "https://data.cdc.gov/resource/3nzu-udr9.json"
-
-# Send a request to the URL
 response = requests.get(url)
 
 # Check if the request was successful
 if response.status_code == 200:
-    # Convert the JSON response to a string
+    # JSON response to a string
     json_str = response.text
-
-    # Convert the string to a pandas DataFrame
+    # string to a DataFrame
     data = pd.read_json(json_str)
-
-    # Display the first few records
-    print("First few records of the dataset:")
+    # display a few records
+    print("A few records of the dataset:")
     print(data.head())
-
-    # Calculate the size and dimensions of the dataset
+    # explore the size and dimensions of the dataset
     print("\nSize and Dimensions of the dataset:")
-    print(f"Number of Rows: {data.shape[0]}")
-    print(f"Number of Columns: {data.shape[1]}")
-    print(f"Total number of elements: {data.size}")
+    print(f"Rows: {data.shape[0]}")
+    print(f"Columns: {data.shape[1]}")
+    print(f"Number of elements: {data.size}")
 
     # Identify missing data
-    print("\nMissing data in each column:")
+    print("\nMissing data in each column?")
     print(data.isnull().sum())
 
     # Write the DataFrame to a CSV file
     data.to_csv('obesity_among_20.csv', index=False)
-    print("Data downloaded and saved as 'obesity_among_20.csv'.")
+    print("Data downloaded and saved.")
 else:
-    print(f"Failed to download the data. Status code: {response.status_code}")
+    print(f"Failed to download with the tatus code: {response.status_code}")
 
 
 
@@ -49,26 +44,26 @@ else:
 
 
 # 2. Web Scraping from HTML data 
-# URL of the page to scrape
+# get response from url
 url_web = 'https://www.cdc.gov/obesity/data/childhood.html'
-# Send a request to the URL
 response_web = requests.get(url_web)
 
-# Check if the request was successful
+
 if response_web.status_code == 200:
-    # Parse the HTML content
+    # parse the HTML content
     soup = BeautifulSoup(response_web.content, 'html.parser')
     
-    # Find the relevant <li> tags
+    # find the relevant content
     obesity_data = soup.find_all('li', string=re.compile('Obesity prevalence was'))
    
-    # Initialize lists to hold the extracted data
+    # lists to hold the extracted data
     age_groups = []
     prevalences_age = []
     ethnicities = []
     prevalences_ethnicity = []
 
-    # Extract data from the first <li> element
+    # extract data
+    # obesity & age
     age_data = obesity_data[0].get_text()
     age_parts = age_data.split(',')
     for part in age_parts:
@@ -77,7 +72,7 @@ if response_web.status_code == 200:
             prevalences_age.append(float(match.group(1)))
             age_groups.append(match.group(2).strip())
     
-    # Extract data from the second <li> element
+    # obesity & age & race
     ethnicity_data = obesity_data[1].get_text()
     ethnicity_parts = ethnicity_data.split(',')
     for part in ethnicity_parts:
@@ -86,15 +81,15 @@ if response_web.status_code == 200:
             prevalences_ethnicity.append(float(match.group(1)))
             ethnicities.append(match.group(2).strip())
 
-    # Convert the extracted data to DataFrames
+    # extracted data to DataFrames
     df_age_group = pd.DataFrame({'Age Group': age_groups, 'Obesity Prevalence (%)': prevalences_age})
     df_ethnicity = pd.DataFrame({'Ethnicity': ethnicities, 'Obesity Prevalence (%)': prevalences_ethnicity})
 
-    # Save the data to CSV files
+    # data to CSV files
     df_age_group.to_csv('obesity_age.csv', index=False)
     df_ethnicity.to_csv('obesity_ethnicity_age.csv', index=False)
 else:
-    print("Failed to retrieve the webpage")
+    print("Failed to retrieve.")
     
 
 
@@ -110,14 +105,14 @@ page_number = 28  # for the 28th page
 with open(file_path, 'rb') as pdf_file:
     pdf_reader = PyPDF2.PdfReader(pdf_file)
     
-    # Extract text from the specific page
+    # extract text from the specific page
     page = pdf_reader.pages[page_number - 1]
     text = page.extract_text()
 
-    # Define the output filename for the extracted text
+    # define the output filename for the extracted text
     output_filename = 'pdf_extracted_data.txt'
 
-    # Save the extracted text to a file
+    # extracted text to .txt
     with open(output_filename, 'w', encoding='utf-8') as output_file:
         output_file.write(text)
     
